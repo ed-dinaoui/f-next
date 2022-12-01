7115167d8a7d9d09d2b1e0e1c70a0f8d18b5d65a
ed-d <80011820+ed-dinaoui@users.noreply.github.com>
2022-12-01 12:52:40 -0500

    Update [ope].js

--- pages/api/[ope].js
+++ pages/api/[ope].js
@@ -43,7 +43,7 @@ export default async function handler ( req , res ) {
         
     youtubedl( URL , Object.assign( options , {
             paths : `/output/`
-        } ) ) ;
+        } ) ).catch(err => console.log(err)) ;
 
     youtubedl( URL , Object.assign( options , {
       dumpSingleJson: true ,
