8552f8d8e4b8c0b85eaaaed2a207daf220dc8990
ed-d <80011820+ed-dinaoui@users.noreply.github.com>
2022-12-01 03:41:24 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 module.exports = {
   reactStrictMode: true ,
   env: {
-    ROOT: process.env.NODE_ENV === 'production' ? `https://f-next.vercel.app` : `https://5wluri-3000.preview.csb.app`
+    ROOT: process.env.NODE_ENV === 'production' ? `https://f-net.netlify.app` : `https://5wluri-3000.preview.csb.app`
   }
+  
 }
